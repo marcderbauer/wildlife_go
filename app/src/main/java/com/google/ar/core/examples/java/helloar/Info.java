147bec6854ac38302d4f60231d74f18316bfb374
marcderbauer <marcanthony.bauer@gmail.com>
2020-11-08 11:28:31 +0100

    First commit

---
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/Info.java
@@ -0,0 +1,16 @@
+package com.google.ar.core.examples.java.helloar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
---
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/MainActivity.java
@@ -0,0 +1,22 @@
+package com.google.ar.core.examples.java.helloar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void enter(View view){
+        Intent intent = new Intent(this, HelloArActivity.class);
+        startActivity(intent);
+    }
+}
